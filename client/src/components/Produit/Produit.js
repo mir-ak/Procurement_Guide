c5391eb674058f8a1bce6f5734b3d6b0ff6b4981
mir-ak <mirakcr7@gmail.com>
2022-12-28 23:36:23 +0100

    update produit

--- client/src/components/Produit/Produit.js
+++ client/src/components/Produit/Produit.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import ReactStars from "react-rating-stars-component";
 export default class Produit extends Component {
   state = {
     commentaire: "",
@@ -17,20 +17,33 @@ export default class Produit extends Component {
     });
   };
 
-  onClickImage = (id) => {
-    window.location.href = this.props.admin + "/showcomment/" + id;
+  onClickImage = (id, category) => {
+    console.log(category);
+    window.location.href =
+      this.props.admin + "/showcomment/" + id + "/" + category;
   };
 
   render() {
-    let { id, name, picture, info, prix } = this.props.item;
+    let { id, title, picture, category, description, price } = this.props.item;
     return (
       <div className="project" style={{ cursor: "pointer" }}>
         <button
           style={{ borderRadius: "25px" }}
-          onClick={() => this.onClickImage(id)}>
+          onClick={() => this.onClickImage(id, category)}>
           <img src={picture} alt="" />
-          <h4>{name}</h4>
-          <h5>{info}</h5>
+          <h4>{title}</h4>
+          <h5>{description}</h5>
+          <ReactStars
+            count={5}
+            size={35}
+            value={3.5}
+            color={"#ffffff"}
+            isHalf={true}
+            emptyIcon={<i className="far fa-star"></i>}
+            halfIcon={<i className="fa fa-star-half-alt"></i>}
+            fullIcon={<i className="fa fa-star"></i>}
+            activeColor="#ffd700"
+          />
           <h6
             style={{
               width: "30%",
@@ -38,7 +51,7 @@ export default class Produit extends Component {
               fontSize: "1.4rem",
               backgroundColor: "green",
             }}>
-            {prix}
+            {price}
           </h6>
         </button>
       </div>
