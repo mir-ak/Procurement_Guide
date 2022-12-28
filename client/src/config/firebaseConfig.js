e7becba1898fe3b60bd09dbd0ec9e9e4014fb23e
mir-ak <mirakcr7@gmail.com>
2022-12-28 18:41:17 +0100

    add storage in firebaseConfig

--- client/src/config/firebaseConfig.js
+++ client/src/config/firebaseConfig.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
+import { getStorage } from "firebase/storage";
 const firebaseConfig = {
   apiKey: "AIzaSyBdIgL7mrTMgjU2d6Tp2t_R2sbfDf48VBU",
   authDomain: "portfolioweb-3106e.firebaseapp.com",
@@ -15,6 +16,8 @@ const firebaseConfig = {
 
 const firebaseApp = initializeApp(firebaseConfig);
 const databaseApp = getDatabase(firebaseApp);
+const storage = getStorage(firebaseApp);
 const auth = getAuth(firebaseApp);
+export { storage };
 export { auth };
 export default databaseApp;
