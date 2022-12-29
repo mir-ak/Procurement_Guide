78566f3167358f65af821a6d81155842e3b1d5bc
mir-ak <mirakcr7@gmail.com>
2022-12-29 01:14:04 +0100

    update diplay comment

--- client/src/page/ShowComment.js
+++ client/src/page/ShowComment.js
@@ -1,19 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { produitData } from "../data/produitData";
 import TextField from "@material-ui/core/TextField";
 import IconButton from "@mui/material/IconButton";
-
 import DirectionsIcon from "@mui/icons-material/Directions";
-
 import Navbar from "../components/Navbar";
 import Commentaire from "../components/commentaire/Commentaire";
-import databaseApp from "../config/firebaseConfig";
-import { ref, push } from "firebase/database";
+import databaseApp, { storage } from "../config/firebaseConfig";
+import * as firebase from "firebase/database";
+import { ref, getDownloadURL } from "firebase/storage";
 
 function ShowComment() {
   const [FromCommentaire, SetCommentaire] = useState({ commentaire: "" });
-  const id = useParams();
+  const [products, setProducts] = useState([]);
+  const qaury = useParams();
   const cancelCourse = () => {
     SetCommentaire({ ...FromCommentaire, commentaire: "" });
   };
@@ -25,7 +24,7 @@ function ShowComment() {
       fetch("/comments?comment=" + FromCommentaire.commentaire)
         .then((res) => res.json())
         .then((data) => {
-          push(ref(databaseApp, "comments/" + id.id), {
+          firebase.push(firebase.ref(databaseApp, "comments/" + qaury.id), {
             commentaire: FromCommentaire.commentaire,
             predire: data["comments"],
           });
@@ -34,47 +33,81 @@ function ShowComment() {
     cancelCourse();
   };
 
-  return (
-    <div className="ShowComment">
-      {id.admin ? <Navbar admin="admin" /> : <Navbar />}
-      <h3>{produitData[id.id].name} </h3>
-      <div className="container">
-        <img src={produitData[id.id].picture} alt="" />
+  useEffect(() => {
+    var newProducts = [];
+    firebase.onValue(
+      firebase.ref(databaseApp, `products/${qaury.category}/${qaury.id}`),
+      (snapshot) => {
+        getDownloadURL(ref(storage, `media/${snapshot.val().picture}`))
+          .then((data) => {
+            let newJsonProduct = {
+              title: snapshot.val().title,
+              description: snapshot.val().description,
+              price: snapshot.val().price,
+              category: snapshot.val().category,
+              picture: data,
+            };
 
-        <Commentaire id={id} />
-      </div>
-      {id.admin ? null : (
-        <div className="box">
-          <h3>Note le produit </h3>
-          <div className="comment"></div>
-          <form className="comment">
-            <TextField
-              variant="outlined"
-              margin="normal"
-              label="Saisir votre Commentaire *"
-              type="text"
-              value={FromCommentaire.commentaire}
-              onChange={(event) =>
-                SetCommentaire({
-                  ...FromCommentaire,
-                  commentaire: event.currentTarget.value,
-                })
-              }
-              InputProps={{
-                endAdornment: (
-                  <IconButton
-                    color="primary"
-                    sx={{ p: "10px" }}
-                    onClick={() => onPressComment()}>
-                    <DirectionsIcon />
-                  </IconButton>
-                ),
-              }}
-            />
-          </form>
+            newProducts.push(newJsonProduct);
+
+            setProducts(newProducts);
+          })
+          .catch((e) => console.log(e));
+      }
+    );
+  }, [qaury]);
+
+  const showOneComment = () => {
+    return (
+      <>
+        {qaury.admin ? null : (
+          <div className="box">
+            <h3>Note le produit </h3>
+            <form className="comment">
+              <TextField
+                variant="outlined"
+                margin="normal"
+                label="Saisir votre Commentaire *"
+                type="text"
+                value={FromCommentaire.commentaire}
+                onChange={(event) =>
+                  SetCommentaire({
+                    ...FromCommentaire,
+                    commentaire: event.currentTarget.value,
+                  })
+                }
+                InputProps={{
+                  endAdornment: (
+                    <IconButton
+                      color="primary"
+                      sx={{ p: "10px" }}
+                      onClick={() => onPressComment()}>
+                      <DirectionsIcon />
+                    </IconButton>
+                  ),
+                }}
+              />
+            </form>
+          </div>
+        )}
+      </>
+    );
+  };
+
+  return (
+    <>
+      {qaury.admin ? <Navbar admin="admin" /> : <Navbar />}
+      {products && products.length > 0 ? (
+        <div className="ShowComment">
+          <h3>{products[0].title} </h3>
+          <div className="container">
+            <img src={products[0].picture} alt="" />
+            <Commentaire id={qaury} />
+          </div>
+          {showOneComment()}
         </div>
-      )}
-    </div>
+      ) : null}
+    </>
   );
 }
 
